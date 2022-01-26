5ba5f2d34a462ce364ee2b5dbae848736523503b
michimichi2021 <michikaho1009@gmail.com>
2022-01-26 20:06:59 +0900

    #15

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>MyBBS</title>
+</head>
+<body>
+    <div class = "container">
+    <h1>My BBS</h1>
+    <ul>
+
+        @forelse ($posts as $index => $post)
+            <li>
+                <a href="/posts/{{ $index }}">
+                    {{ $post }}
+                </a>
+            </li>
+        @empty
+            <li>No posts yet!</li>
+        @endforelse
+    </ul>
+    </div>
+</body>
+</html>
