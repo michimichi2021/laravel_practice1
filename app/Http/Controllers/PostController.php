73739b48d610f36a9b0e83395ded62d922768a7f
michimichi2021 <michikaho1009@gmail.com>
2022-01-27 17:28:40 +0900

    データベースの勉強

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,25 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-
-    private $posts = [
-        'Title A',
-        'Title B',
-        'Title C',
-    ];
-
-    public function index() {
+    public function index()
+    {
+        $posts = Post::latest()->get();
 
         return view('index')
-            ->with(['posts' => $this->posts]);
+            ->with(['posts' => $posts]);
     }
 
-    public function show($id)
+
+    public function show(Post $post)
     {
+
         return view('posts.show')
-            ->with(['post' => $this->posts[$id]]);
+            ->with(['post' => $post]);
     }
 }
